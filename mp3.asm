185c14549fecfda53a602a4231c66515aa79439d
riggsybanez <142960686+riggsybanez@users.noreply.github.com>
2024-03-26 10:08:49 +0800

    Update mp3.asm

--- mp3.asm
+++ mp3.asm
@@ -1,1 +1,33 @@
+section .bss
+    buffer resb 1
+
+section .text
+    global _start
+
+_start:
+    
+    mov eax, 3           
+    mov ebx, 0           
+    mov ecx, buffer      
+    mov edx, 1          
+    int 80h             
+ 
+    cmp byte [buffer], 'a'
+    jl character_not_lowercase
+    cmp byte [buffer], 'z'
+    jg character_not_lowercase
+
+
+    sub byte [buffer], 32  
+
+character_not_lowercase:
+
+    mov eax, 4           
+    mov ebx, 1          
+    mov ecx, buffer      
+    mov edx, 1           
+    int 80h            
+
+
+    mov eax, 1           
 
